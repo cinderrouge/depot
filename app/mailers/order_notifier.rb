1f8168d4980f610f92038ba50f884ed669935616
Cinder Rouge <marlene.reddy@gmail.com>
2017-07-12 13:15:02 +1200

    smtp

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  default from: "Marlene <orders@example.com>"
+  default from: "Marlene <ocinder.rouge@gmail.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
